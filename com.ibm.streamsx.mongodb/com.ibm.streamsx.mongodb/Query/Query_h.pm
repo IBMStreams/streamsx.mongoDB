ef85f116400e7ceb23e171ea14097682a7dff4ef
leongor <leongor@gmail.com>
2018-07-09 21:25:50 +0300

    Added SSL support to Insert/Query operators.

--- com.ibm.streamsx.mongodb/com.ibm.streamsx.mongodb/Query/Query_h.pm
+++ com.ibm.streamsx.mongodb/com.ibm.streamsx.mongodb/Query/Query_h.pm
@@ -1,3 +1,5 @@
+# SPL_CGT_INCLUDE: ../Common/MongoInclude_h.cgt
+# SPL_CGT_INCLUDE: ../Common/MongoInit_h.cgt
 
 package Query_h;
 use strict; use Cwd 'realpath';  use File::Basename;  use lib dirname(__FILE__);  use SPL::Operator::Instance::OperatorInstance; use SPL::Operator::Instance::Annotation; use SPL::Operator::Instance::Context; use SPL::Operator::Instance::Expression; use SPL::Operator::Instance::ExpressionTree; use SPL::Operator::Instance::ExpressionTreeEvaluator; use SPL::Operator::Instance::ExpressionTreeVisitor; use SPL::Operator::Instance::ExpressionTreeCppGenVisitor; use SPL::Operator::Instance::InputAttribute; use SPL::Operator::Instance::InputPort; use SPL::Operator::Instance::OutputAttribute; use SPL::Operator::Instance::OutputPort; use SPL::Operator::Instance::Parameter; use SPL::Operator::Instance::StateVariable; use SPL::Operator::Instance::TupleValue; use SPL::Operator::Instance::Window; 
@@ -7,7 +9,9 @@ sub main::generate($$) {
    my $model = SPL::Operator::Instance::OperatorInstance->new($$xml);
    unshift @INC, dirname ($model->getContext()->getOperatorDirectory()) . "/../impl/nl/include";
    $SPL::CodeGenHelper::verboseMode = $model->getContext()->isVerboseModeOn();
+   print '#include <SPL/Runtime/Function/SPLFunctions.h>', "\n";
    print '#include <SPL/Runtime/Operator/OperatorMetrics.h>', "\n";
+   print '#include <streams_boost/shared_ptr.hpp>', "\n";
    print '#include <streams_boost/thread/tss.hpp>', "\n";
    print '#include <streams_boost/typeof/typeof.hpp>', "\n";
    print '#include <streams_boost/foreach.hpp>', "\n";
@@ -16,19 +20,47 @@ sub main::generate($$) {
    print '#include "Mongo.h"', "\n";
    print "\n";
    print 'using std::string;', "\n";
+   print 'using streams_boost::shared_ptr;', "\n";
    print "\n";
    print 'using namespace mongo;', "\n";
    print "\n";
+   print '#include "Mongo.h"', "\n";
+   print "\n";
+   print "\n";
+   print "\n";
    SPL::CodeGen::headerPrologue($model);
+   print "\n";
+   print "\n";
+   	my $sslOptions = ($_ = $model->getParameterByName('sslOptions')) ? $_->getValueAt(0)->getCppExpression() : '';
+   	my $sslOptionsType = ($_ = $model->getParameterByName('sslOptions')) ? $_->getValueAt(0)->getCppType() : '';
+   	
+   	my $clientOptions ="client::Options().setSSLMode( (client::Options::SSLModes)(int)sslOptions.get_sslMode())" .
+   										".setSSLCAFile( sslOptions.get_caFile())" .
+   										".setSSLPEMKeyFile( sslOptions.get_pemKeyFile())" .
+   										".setSSLPEMKeyPassword( sslOptions.get_pemKeyPassword())" .
+   										".setSSLCRLFile( sslOptions.get_crlFile())" .
+   										".setSSLAllowInvalidCertificates( sslOptions.get_allowInvalidCertificates())" .
+   										".setSSLAllowInvalidHostnames( sslOptions.get_allowInvalidHostnames())" if ($sslOptions);
    print "\n";
    print "\n";
    print 'template<class Void>', "\n";
    print 'struct MongoInit {', "\n";
    print '	static Status status_;', "\n";
+   print '	', "\n";
+   print 'private:', "\n";
+   print '	static Status init(';
+   print "$sslOptionsType const& sslOptions" if($sslOptionsType);
+   print ' ) {', "\n";
+   print '		return client::initialize(';
+   print $clientOptions;
+   print ');', "\n";
+   print '	}', "\n";
    print '};', "\n";
    print "\n";
    print 'template<class Void>', "\n";
-   print 'Status MongoInit<Void>::status_ = client::initialize();', "\n";
+   print 'Status MongoInit<Void>::status_ = init(';
+   print $sslOptions;
+   print ');', "\n";
    print "\n";
    print 'class MY_OPERATOR: public MY_BASE_OPERATOR {', "\n";
    print "\n";
